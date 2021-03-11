e73561e875c98301510df3f95e353938bf4f1517
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-03-11 23:17:41 +0600

    created a product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './Product.css'
 const Product = (props) => {
+    console.log(props.product);
+    const {name,img,seller,price,stock}=props.product;
     return (
-        <div>
-            <h3>This is a product</h3>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div className='product-details'>
+            <h4 className='product-name'>{name}</h4>
+            <p><small>seller : {seller}</small></p>
+            <p>${price}</p>
+            <br/>
+            <p>only {stock} items left! Don't be late</p>
+
+            </div>
+            
         </div>
     );
 };
