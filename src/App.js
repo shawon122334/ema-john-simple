e68dea8d41d921a36598dbdcfdfbabe16ddb5f4c
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-04-08 17:59:37 +0600

    route link added on product name

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import Review from './components/Review/Review';
 import Inventory from './components/Inventory/Inventory';
 import NoMatch from './components/NoMatch/NoMatch';
+import ProductDetails from './components/Product/ProductDetails';
 
 function App() {
   return (
@@ -29,6 +30,9 @@ function App() {
         </Route>
         <Route exact path='/'>
           <Shop></Shop>
+        </Route>
+        <Route path='/product/:productKey'>
+          <ProductDetails></ProductDetails>
         </Route>
         <Route path="*">
           <NoMatch></NoMatch>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+import { Link } from 'react-router-dom';
 const Product = (props) => {
     // console.log(props.product);
-    const {name,img,seller,price,stock}=props.product;
+    const {name,img,seller,price,stock,key}=props.product;
     return (
         <div className="product">
             <div>
                 <img src={img} alt=""/>
             </div>
             <div className='product-details'>
-            <h4 className='product-name'>{name}</h4>
+            <h4 className='product-name'> <Link to={'/product/'+key}>{name}</Link> </h4>
             <p><small>seller : {seller}</small></p>
             <p>${price}</p>
             <p>only {stock} items left! Don't be late</p>
